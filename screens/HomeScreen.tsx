5bba95484e0baa886756e757a15e3a937deeac30
hung0968374@gmail.com <hung0968374@gmail.com>
2021-03-18 11:16:39 +0700

    'asdf'

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,55 +1,178 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
-import { Text, View } from '../components/Themed';
-import { StyleSheet, Image } from "react-native";
+import { Text, View } from "../components/Themed";
+import { StyleSheet, Image, TouchableOpacity } from "react-native";
 
-import CartApi from '../api/CartApi'
-
-import Header from '../components/Header';
-import FlashSale from '../components/FlashSale';
-import { AxiosResponse } from 'axios';
+import CartApi from "../api/CartApi";
+import { Ionicons } from "@expo/vector-icons";
+import Header from "../components/Header";
+import FlashSale from "../components/FlashSale";
+import { AxiosResponse } from "axios";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
-  const [flashSale, setFlashSale] = useState<AxiosResponse | null | void>(null)
-
+  const [flashSale, setFlashSale] = useState<AxiosResponse | null | void>(null);
+  const navigation = useNavigation();
+  const [lastPage, setLastPage] = useState(50);
+  const [PrimaryPage, setPrimaryPage] = useState(5);
+  {
+    /*primaryPage la trang dau tien trong danh sach cac trang*/
+  }
+  const [currentPage, setCurrentPage] = useState(1);
+  {
+    /*currentPage la trang hien tai trong danh sach cac trang*/
+  }
+  const [pages, setPages] = useState([
+    PrimaryPage,
+    PrimaryPage + 1,
+    PrimaryPage + 2,
+    PrimaryPage + 3,
+    "...",
+    lastPage,
+  ]);
+  console.log(pages);
   useEffect(() => {
     async function fetchFlashSale() {
       try {
-        const data = await CartApi.getAll()
-        setFlashSale(data)
+        const data = await CartApi.getAll();
+        setFlashSale(data);
       } catch (error) {
         console.log(error);
       }
     }
 
-    fetchFlashSale()
-
-  }, [])
-
+    fetchFlashSale();
+    setPages([
+      PrimaryPage,
+      PrimaryPage + 1,
+      PrimaryPage + 2,
+      PrimaryPage + 3,
+      "...",
+      lastPage,
+    ]);
+  }, [lastPage, PrimaryPage]);
   return (
     <View>
       <Header />
       <View style={styles.cards}>
         <Text style={styles.title}>Sản phẩm mới về</Text>
-        <Image style={styles.image} source={require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")} />
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("TopNav", { screen: "NotificationsScreen" })
+          }
+        >
+          <Image
+            style={styles.image}
+            source={require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")}
+          />
+        </TouchableOpacity>
       </View>
       <FlashSale list={flashSale} />
-
 
       {/* <SafeAreaView style={styles.container}>
         <CarouselCards />
       </SafeAreaView> */}
-
+      <View style={styles.pagination}>
+        <View style={styles.alignCenter}>
+          <TouchableOpacity
+            onPress={() => {
+              setPrimaryPage(1);
+            }}
+          >
+            <Ionicons name="play-back-circle-outline" size={30}></Ionicons>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.alignCenter}>
+          <TouchableOpacity
+            onPress={() => {
+              if (PrimaryPage > 1) setPrimaryPage(PrimaryPage - 1);
+            }}
+          >
+            <Ionicons name="caret-back-circle-outline" size={30}></Ionicons>
+          </TouchableOpacity>
+        </View>
+        {pages.map((page, index) => (
+          <TouchableOpacity
+            onPress={() => {
+              if (page != "...") {
+                setCurrentPage(page);
+                if (PrimaryPage == page) {
+                  if (PrimaryPage > 2) setPrimaryPage(page - 2);
+                  else if (PrimaryPage == 2) setPrimaryPage(1);
+                } else if (PrimaryPage + 3 == page)
+                  if (PrimaryPage < lastPage - 4) setPrimaryPage(page);
+              }
+            }}
+            key={index}
+          >
+            {page == currentPage ? (
+              <View style={styles.specialPage}>
+                <Text>{page}</Text>
+              </View>
+            ) : (
+              <View style={styles.eachPage}>
+                <Text>{page}</Text>
+              </View>
+            )}
+          </TouchableOpacity>
+        ))}
+        <View style={styles.alignCenter}>
+          <TouchableOpacity
+            onPress={() => {
+              if (PrimaryPage < lastPage - 4)
+                setPrimaryPage((prev) => prev + 1);
+            }}
+          >
+            <Ionicons name="caret-forward-circle-outline" size={30}></Ionicons>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.alignCenter}>
+          <TouchableOpacity
+            onPress={() => {
+              setPrimaryPage(lastPage - 4);
+            }}
+          >
+            <Ionicons name="play-forward-circle-outline" size={30}></Ionicons>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   cards: {
-    marginBottom: 10
+    marginBottom: 10,
   },
 
+  pagination: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  alignCenter: {
+    justifyContent: "center",
+  },
+  eachPage: {
+    width: 30,
+    height: 30,
+    justifyContent: "center",
+    borderColor: "black",
+    borderWidth: 1,
+    alignItems: "center",
+    backgroundColor: "#32a852",
+    borderRadius: 50,
+  },
+  specialPage: {
+    width: 30,
+    height: 30,
+    justifyContent: "center",
+    borderColor: "black",
+    borderWidth: 1,
+    alignItems: "center",
+    backgroundColor: "red",
+    borderRadius: 50,
+  },
   image: {
     resizeMode: "cover",
     width: "100%",
@@ -63,9 +186,9 @@ const styles = StyleSheet.create({
   },
 
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 50
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50,
   },
 });
--- screens/NotificationsScreen.tsx
+++ screens/NotificationsScreen.tsx
@@ -1,24 +1,254 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '../components/Themed';
+import * as React from "react";
+import { useState } from "react";
+import { FlatList, StyleSheet, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Text, View } from "../components/Themed";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function NotificationsScreen() {
+  const [amountOfCmt, setAmountOfCmt] = useState(125);
+  const [comment, setComment] = useState([
+    {
+      user: "hung",
+      id: 1,
+      cmt: "con hang nay ngon lam",
+    },
+    {
+      user: "hung",
+      id: 2,
+      cmt: "con hang nay ngon lam",
+    },
+    {
+      user: "hung",
+      id: 3,
+      cmt:
+        "con hang nay ngon lamaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    },
+    {
+      user: "hung",
+      id: 4,
+      cmt: "con hang nay ngon lam",
+    },
+    {
+      user: "hung",
+      id: 5,
+      cmt: "con hang nay ngon lam",
+    },
+  ]);
+  const imgs = [
+    {
+      id: 1,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 2,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 3,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 4,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 5,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 6,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 7,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 8,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 9,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 10,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 11,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 12,
+      ulr:
+        "../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg",
+    },
+    {
+      id: 13,
+      ulr:
+        'require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")',
+    },
+  ];
+  console.log({ comment });
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>NotificationsScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.scrollArea}>
+          <ScrollView horizontal={true}>
+            <Image
+              style={styles.image}
+              resizeMode={"contain"}
+              source={require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")}
+            ></Image>
+            <Image
+              style={styles.image}
+              resizeMode={"contain"}
+              source={require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")}
+            ></Image>
+            <Image
+              style={styles.image}
+              resizeMode={"contain"}
+              source={require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")}
+            ></Image>
+            <Image
+              style={styles.image}
+              resizeMode={"contain"}
+              source={require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")}
+            ></Image>
+            <Image
+              style={styles.image}
+              resizeMode={"contain"}
+              source={require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")}
+            ></Image>
+          </ScrollView>
+        </View>
+
+        <View>
+          <View style={styles.productNamePrice}>
+            <Text style={{ fontSize: 20 }}>Laptop abc</Text>
+            <Text style={{ fontSize: 20 }}>
+              Gia: <Text style={styles.boldText}>20.000.000d</Text>
+            </Text>
+          </View>
+          <Text style={{ fontSize: 18 }}>Cau hinh</Text>
+        </View>
+        <View style={styles.commentSection}>
+          <Text>Nhan xet: ({amountOfCmt})</Text>
+          <View>
+            {comment.map((item) => (
+              <View key={item.id} style={styles.userComment}>
+                <Text style={[{ flex: 2, fontWeight: "bold" }, styles.text16]}>
+                  {item.user}
+                </Text>
+                <Text style={[{ flex: 9 }, styles.text16]}>{item.cmt}</Text>
+              </View>
+            ))}
+          </View>
+          <Text style={[styles.text20, styles.watchFurther]}>
+            Xem them{" "}
+            <Ionicons name="arrow-forward-outline" size={20}></Ionicons>
+          </Text>
+        </View>
+        {/* mat hang lien quan */}
+        <View>
+          <Text
+            style={[
+              styles.centerItem,
+              styles.text16,
+              { backgroundColor: "#4287f5" },
+            ]}
+          >
+            Co the ban cung thich
+          </Text>
+          <View style={styles.moreItemContainer}>
+            {imgs.map((img, index) => (
+              <View key={index} style={styles.eachPost}>
+                <Image
+                  style={styles.moreItemImg}
+                  source={require("../assets/images/window-desk-watches-notebook-smartphone-headphones.jpg")}
+                ></Image>
+                <Text>Gia: 1 trieu</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  moreItemContainer: {
+    marginLeft: 8,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  eachPost: {
+    marginRight: 8,
+    marginTop: 5,
+  },
+  moreItemImg: {
+    width: 184,
+    height: 150,
+  },
+  centerItem: {
+    textAlign: "center",
+  },
+  text16: {
+    fontSize: 16,
+  },
+  watchFurther: {
+    margin: 8,
+    textAlign: "center",
+    backgroundColor: "#cccccc",
+  },
+  text20: {
+    fontSize: 20,
+  },
+  scrollArea: {
+    height: 245,
+    backgroundColor: "rgba(52, 52, 52, 0.8)",
+  },
+  commentSection: {
+    marginTop: 5,
+    marginBottom: 5,
+    height: "auto",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  image: {
+    width: 370,
+    height: "auto",
+    marginLeft: 5,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  productNamePrice: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  userComment: {
+    flexDirection: "row",
+    marginLeft: 8,
+    marginRight: 8,
   },
 });
