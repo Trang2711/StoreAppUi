58da808b09379b3e7516a6d2a02abf629720b70d
TrangTrinh <trinhtrang27112000@gmail.com>
2021-05-22 14:56:55 +0700

    import expo image picker

--- api/ProductApi.tsx
+++ api/ProductApi.tsx
@@ -13,12 +13,12 @@ const ProductApi = {
     const url = `/api/flashsale_product`
     return AxiosClient.get(url)
   },
-  
+
   getSpecifiedProduct: (id: any) => {
     const url = `/api/product/${id}`;
     return AxiosClient.get(url);
   },
-  
+
   getCommentsOfProduct: (data: any) => {
     const url = `/api/get_review`
     return AxiosClient.post(url, data)
